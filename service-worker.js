c51e12178886247df407df15e63c898cafa34f9b
karen102_14 <karen102_14@hotmail.com>
2023-09-11 03:31:52 -0400

    service worker test

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
  //Update cache names any time any of the cached files change.
- const CACHE_NAME = 'static-cache-v10';
+ const CACHE_NAME = 'static-cache-v11';
 
   //Add list of files to cache here.
   const FILES_TO_CACHE = [
@@ -69,7 +69,7 @@ self.addEventListener('fetch', (evt) => {
             .catch(() => {
                 return caches.open(CACHE_NAME)
                     .then((cache) => {
-                        return cache.match('/offline.html');
+                        return cache.match('/TP3_PoulinLarochelleKaren/offline.html');
                     });
             })
     );
