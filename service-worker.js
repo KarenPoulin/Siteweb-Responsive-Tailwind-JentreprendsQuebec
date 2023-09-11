403e92384b52519daf778220d5c344d893930210
karen102_14 <karen102_14@hotmail.com>
2023-09-11 02:54:24 -0400

    updated service worker

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-//Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v6';
+ //Update cache names any time any of the cached files change.
+ const CACHE_NAME = 'static-cache-v7';
 
-//Add list of files to cache here.
-const FILES_TO_CACHE = [
+  //Add list of files to cache here.
+  const FILES_TO_CACHE = [
     'offline.html',
     'styles/css/normalize.css',
     'styles/css/style.css',
@@ -25,21 +25,20 @@ const FILES_TO_CACHE = [
 
 self.addEventListener('install', (evt) => {
     console.log('[ServiceWorker] Install');
-    // Precache static resources here.
-    evt.waitUntil(
+     // Precache static resources here.
+     evt.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             console.log('[ServiceWorker] Pre-caching offline page');
             return cache.addAll(FILES_TO_CACHE);
         })
-    );
-    self.skipWaiting();
+     );
+    self.skipWaiting(); 
 });
 
 self.addEventListener('activate', (evt) => {
     console.log('[ServiceWorker] Activate');
-
-    //Remove previous cached data from disk.
-    evt.waitUntil(
+     //Remove previous cached data from disk.
+     evt.waitUntil(
         caches.keys().then((keyList) => {
             return Promise.all(keyList.map((key) => {
                 if (key !== CACHE_NAME) {
@@ -48,23 +47,25 @@ self.addEventListener('activate', (evt) => {
                 }
             }));
         })
-    );
-    self.clients.claim();
+     );
+
+     self.clients.claim();
 });
+
 self.addEventListener('fetch', (evt) => {
     console.log('[ServiceWorker] Fetch', evt.request.url);
-    //Add fetch event handler here.
-    if (evt.request.mode !== 'navigate') {
+     //Add fetch event handler here.
+     if (evt.request.mode !== 'navigate') {
         // Not a page navigation, bail.
         return;
-    }
-    evt.respondWith(
+     }
+     evt.respondWith(
         fetch(evt.request)
-            .catch(() => {
+        .catch(() => {
             return caches.open(CACHE_NAME)
-                .then((cache) => {
-                return cache.match('/KarenPoulin/TP3_PoulinLarochelleKaren/offline.html' );
+            .then((cache) => {
+                return cache.match('/KarenPoulin/TP3_PoulinLarochelleKaren/offline.html');
             });
         })
-    );
+     );
 });
