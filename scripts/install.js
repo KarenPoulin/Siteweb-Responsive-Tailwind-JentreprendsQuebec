630d722e39261dac882c7eca31ab9ab63c64b3a5
karen102_14 <karen102_14@hotmail.com>
2023-09-15 15:09:34 -0400

    pwa test

--- scripts/install.js
+++ scripts/install.js
@@ -1,47 +1,39 @@
 let deferredInstallPrompt = null;
-const installButton = document.getElementById('butInstall');
-installButton.addEventListener('click', installPWA);
+const installButton = document.getElementById("butInstall");
 
+installButton.addEventListener("click", installPWA);
 
-// Add event listener for beforeinstallprompt event
-window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+window.addEventListener("beforeinstallprompt", saveBeforeInstallPromptEvent);
 
 function saveBeforeInstallPromptEvent(evt) {
-    // CODELAB: Add code to save event & show the install button.
-        deferredInstallPrompt = evt;
-        installButton.removeAttribute('hidden');
-    }
+	deferredInstallPrompt = evt;
+	installButton.removeAttribute("hidden");
+}
 
 function installPWA(evt) {
-    console.log('click');
-        // Add code show install prompt & hide the install button.
-        deferredInstallPrompt.prompt();
-        // Hide the install button, it can't be called twice.
-        evt.srcElement.setAttribute('hidden', true);
-
-
-        // Log user response to prompt.
-        deferredInstallPrompt.userChoice
-            .then((choice) => {
-                if (choice.outcome === 'accepted') {
-                    console.log('User accepted the A2HS prompt', choice);
-                } else {
-                    console.log('User dismissed the A2HS prompt', choice);
-                }
-                deferredInstallPrompt = null;
-            });
-    }
-
-
-// Add event listener for appinstalled event
+	// CODELAB: Add code show install prompt & hide the install button.
+	deferredInstallPrompt.prompt();
+	evt.srcElement.setAttribute("hidden", true);
+	// CODELAB: Log user response to prompt.
+	deferredInstallPrompt.userChoice.then((choice) => {
+		if (choice.outcome === "accepted") {
+			console.log("L'utilisateur a installé la PWA via le bouton.", choice);
+		} else {
+			console.log("L'utilisateur a refusé d'installer la PWA.", choice);
+		}
+		deferredInstallPrompt = null;
+	});
+}
+// CODELAB: Add event listener for appinstalled event
 window.addEventListener('appinstalled', logAppInstalled);
-/**
-* Event handler for appinstalled event.
-* Log the installation to analytics or save the event somehow.
-*
-* @param {Event} evt
-*/
+
+/*
+ * Event handler for appinstalled event.
+ * Log the installation to analytics or save the event somehow.
+ *
+ * @param {Event} evt
+ */
 function logAppInstalled(evt) {
-// Add code to log the event
-console.log('JEQ App was installed.', evt);
+	// CODELAB: Add code to log the event
+    console.log('Jentreprends Quebec was installed.', evt);
 }
